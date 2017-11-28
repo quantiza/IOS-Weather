2a0d53c43a22358cd357005430ee0ce9150e16a9
quant <923265872@qq.com>
2017-11-28 17:24:04 +0800

    新建项目

---
+++ Weather/Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by LongYang on 2017/11/28.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weather/Weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by LongYang on 2017/11/28.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
